810e637e69d8ccf44da00ada6084383acd21a2cb
Kristiyan Tishev <kristiyan.tishev@mentormate.com>
2025-02-19 17:30:22 +0200

    add implementations for basic sorting algorithms

---
+++ sortingAlgorithms/mergeSort.js
@@ -0,0 +1,37 @@
+function merge(arr1, arr2) {
+  const result = [];
+  let i = 0;
+  let j = 0;
+
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] > arr2[j]) {
+      result.push(arr2[j]);
+      j++;
+    } else {
+      result.push(arr1[i]);
+      i++;
+    }
+  }
+
+  while (i < arr1.length) {
+    result.push(arr1[i]);
+    i++;
+  }
+
+  while (j < arr2.length) {
+    result.push(arr2[j]);
+    j++;
+  }
+
+  return result;
+}
+
+function mergeSort(arr) {
+  if (arr.length < 2) return arr;
+
+  const mid = Math.floor(arr.length / 2);
+  const left = mergeSort(arr.slice(0, mid));
+  const right = mergeSort(arr.slice(mid));
+
+  return merge(left, right);
+}
